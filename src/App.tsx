7a833dc1e3afe151f5489cde1f95162366760a07
pydev <adam.romanski.512@hotmail.com>
2024-05-08 09:33:32 +0200

    fix url

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <Routes>
-      <Route   path='/home'  element={<Home/>} />
+      <Route   path='/'  element={<Home/>} />
       {/* <Route   path='/exchange'  element={<Exchange/>} />
       <Route   path='/swap'  element={<Swap/>} /> */}
      
